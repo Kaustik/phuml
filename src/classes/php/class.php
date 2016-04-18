d9efebcc43be6ccf91a890978d22f80382648fbc
Christian Sjöström <christian@kaustik.com>
2016-04-18 16:59:47 +0200

    refactor

--- src/classes/php/class.php
+++ src/classes/php/class.php
@@ -1,37 +1,22 @@
 <?php
 
-class plPhpClass
-{
-    private $properties;
+use Phuml\Generator\PhpProperty;
 
-    public function __construct( $name, $attributes = array(), $functions = array(), $implements = array(), $extends = null ) 
-    {
-        $this->properties = array( 
-            'name'          =>  $name,
-            'attributes'    =>  $attributes,
-            'functions'     =>  $functions,
-            'implements'    =>  $implements,
-            'extends'       =>  $extends,
+class plPhpClass extends PhpProperty
+{
+    public function __construct(
+        $name,
+        $attributes = array(),
+        $functions = array(),
+        $implements = array(),
+        $extends = null
+    ) {
+        $this->properties = array(
+            'name' => $name,
+            'attributes' => $attributes,
+            'functions' => $functions,
+            'implements' => $implements,
+            'extends' => $extends,
         );
     }
-
-    public function __get( $key )
-    {
-        if ( !array_key_exists( $key, $this->properties ) )
-        {
-            throw new plBasePropertyException( $key, plBasePropertyException::READ );
-        }
-        return $this->properties[$key];
-    }
-
-    public function __set( $key, $val )
-    {
-        if ( !array_key_exists( $key, $this->properties ) )
-        {
-            throw new plBasePropertyException( $key, plBasePropertyException::WRITE );
-        }
-        $this->properties[$key] = $val;            
-    }
 }
-
-?>
