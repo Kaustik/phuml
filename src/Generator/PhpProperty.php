dded5a424e42f13993de4753dc45d4739c927441
Christian Sjöström <christian@kaustik.com>
2016-04-26 15:49:45 +0200

    not needed anymore?

--- src/Generator/PhpProperty.php
+++ src/Generator/PhpProperty.php
@@ -10,7 +10,7 @@ class PhpProperty
         if (!empty($namespace) && strlen($namespace) > 1) {
             $namespace .= '\\';
         }
-        $namespace = str_replace('\\','\\\\',$namespace);
+        #$namespace = str_replace('\\','\\\\',$namespace);
         return $namespace.$this->name;
     }
 }
